d766d8415bba823cfed20cb02ee2b445f778834c
Rezaul Karim <reza1381996@gmail.com>
2022-12-04 12:47:50 +0600

    login form added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import banner from "../../images/istockphoto-1321277096-612x612 1.png";
+import logo from "../../images/Vector.svg";
 
 const Login = () => {
-    return (
-        <div className='mt-24 text-cyan-500' >
-            <h1 className='text-5xl'>Log in Form</h1>
-            
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleForm = (e) =>{
+    e.preventDefault();
+    console.log(email, password);
+  }
+
+  return (
+    <div className="main">
+      <div className="img_container">
+        <img className="logo" src={logo} alt="" />
+        <img className="banner" src={banner} alt="" />
+      </div>
+      <div className="form_container">
+        <h1 className=" ">Log in Form</h1>
+        <form>
+          <input
+            onBlur={handleEmail}
+            className=" border-b-2 border-gray-300 block"
+            type="email"
+            placeholder="Write Email Address"
+          />
+          <div>
+            <input
+              onBlur={handlePassword}
+              className=" border-b-2 border-gray-300 block"
+              type="password"
+              placeholder="Write password"
+            />
+            <small className="pass_instruction">
+              Your password must be 8 character
+            </small>
+          </div>
+          <div className="btn_container">
+            <button onClick={handleForm} className="signup_btn">
+              Log In
+            </button>
+          </div>
+          <p className="toggle_para">
+            Don’t have an account? &nbsp;
+            <span className="login_span">
+              <Link to="/">SIGNUP HERE!</Link>
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -3,13 +3,35 @@ import banner from "../../images/istockphoto-1321277096-612x612 1.png";
 import logo from "../../images/Vector.svg";
 import './SignUp.css';
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [firstName, setFirstName] = useState('');
-  const [lastName, setlastName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [number, setNumber] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleFirstName =(e)=>{
+     setFirstName(e.target.value);
+  }
+  const handleLastName =(e)=>{
+     setLastName(e.target.value);
+  }
+  const handlePhone =(e)=>{
+    setNumber(e.target.value);
+  }
+  const handleEmail =(e)=>{
+    setEmail(e.target.value);
+  }
+  const handlePassword =(e)=>{
+    setPassword(e.target.value);
+  }
+
+  const handleForm = (e) =>{
+    e.preventDefault();
+    console.log(firstName, lastName, email, number, password);
+  }
 
   return (
     <div className="main">
@@ -19,21 +41,21 @@ const SignUp = () => {
       </div>
       <div className="form_container">
         <h1 className=" ">Sign Up Form</h1>
-        <form>
-            <input className=" border-b-2 border-gray-300 block" type="text" placeholder="Write First Name" />
-            <input className=" border-b-2 border-gray-300 block" type="text" placeholder="Write Last Name" />
-            <input className=" border-b-2 border-gray-300 block" type="number" placeholder="+880   1xxxxxxxx" />
-            <input className=" border-b-2 border-gray-300 block" type="email" placeholder="Write Email Address" />
+        <form >
+            <input onBlur={handleFirstName} className=" border-b-2 border-gray-300 block" type="text" placeholder="Write Last Name" />
+            <input onBlur={handleLastName} className=" border-b-2 border-gray-300 block" type="text" placeholder="Write First Name" />
+            <input onBlur={handlePhone} className=" border-b-2 border-gray-300 block" type="number" placeholder="+880   1xxxxxxxx" />
+            <input onBlur={handleEmail} className=" border-b-2 border-gray-300 block" type="email" placeholder="Write Email Address" />
             <div>
-              <input className=" border-b-2 border-gray-300 block" type="password" placeholder="Write password" />
+              <input onBlur={handlePassword} className=" border-b-2 border-gray-300 block" type="password" placeholder="Write password" />
               <small className="pass_instruction">Your password must be 8 character</small>
             </div>
             <div className="btn_container">
               <button className="back_btn">Back</button>
-              <button className="signup_btn">Next Step <FaArrowRight></FaArrowRight></button>
+              <button onClick={handleForm} className="signup_btn">Next Step <FaArrowRight></FaArrowRight></button>
               <button className="signup_btn">Submit <FaArrowRight></FaArrowRight></button>
             </div>
-            <p className="toggle_para">Already have an account? &nbsp;<span className="login_span"> LOGIN HERE!</span></p>
+            <p className="toggle_para">Already have an account? &nbsp;<span className="login_span"><Link to='/login'>LOGIN HERE!</Link></span></p>
         </form>
       </div>
     </div>
